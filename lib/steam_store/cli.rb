6bea06416ab42c53e8b8ba53acfa966c52e9eeb8
Justin Kenney <justinkenney13@gmail.com>
2018-10-24 22:32:01 +0000

    added print_game and debugged price

--- lib/steam_store/cli.rb
+++ lib/steam_store/cli.rb
@@ -28,9 +28,9 @@ class SteamStore::CLI
 
     puts "Welcome to the Steam Store"
     create_games
-    # binding.pry
-    game_info(0)
-    # menu
+
+    # game_info(0)
+    menu
 
   end
 
@@ -40,17 +40,47 @@ class SteamStore::CLI
   end
 
   def menu
-    puts "Which game would you like to know more about?"
     SteamStore::Game.all.pop
-    SteamStore::Game.all.each.with_index(1) {|game, index| puts "#{index}. #{game.name}"}
     input = nil
     until input == "exit"
-      input = gets.chomp
+      puts "Which game would you like to know more about? [Enter a number or 'exit']"
+      puts ""
+      SteamStore::Game.all.each.with_index(1) {|game, index| puts "#{index}. #{game.name}"}
+      input = gets.strip
+
+      game = SteamStore::Game.all[input.to_i-1]
+      game_info(game)
+      print_game(game)
+
+      puts ""
+      puts "Would you like to know about a different game? [y/n]"
+      input = gets.strip.downcase
+
+      if input == "y"
+        menu
+      elsif input == "n"
+        puts "See ya!"
+        exit
+      else
+        menu
+      end
     end
   end
 
-  def game_info(index)
-    url = SteamStore::Game.all[index.to_i].url
-    SteamStore::Game.all[index.to_i].add_info(SteamStore::Scraper.scrape_game(url))
+  def game_info(game)
+    game.add_info(SteamStore::Scraper.scrape_game(game.url))
+  end
+
+  def print_game(game)
+    puts ""
+    puts "#{game.name}"
+    puts ""
+    puts "Developed by #{game.developer} and was released on #{game.release_date}."
+    puts "It is currently #{game.sale} for #{game.price}"
+    puts ""
+    puts "About: #{game.summary}"
+    puts ""
+    puts "Category: #{game.category}"
+    puts "For more information please visit #{game.url}"
   end
 end
--- lib/steam_store/scraper.rb
+++ lib/steam_store/scraper.rb
@@ -6,8 +6,6 @@ class SteamStore::Scraper
 
   def scrape_for_content
     new_releases = home_page.search("#tab_newreleases_content a")
-    # name = new_releases.search(".tab_item_name").text
-    # url = new_releases.attribute("href").value
     new_releases.collect {|game| h = {:name => game.search(".tab_item_name").text,
       :url => game.attribute("href").value}}
   end
@@ -21,10 +19,10 @@ class SteamStore::Scraper
     h[:category] = game.search(".blockbg a:nth-of-type(2)").text
     if game.search(".price").text == ""
       h[:price] = game.search(".discount_final_price").first.text
-      h[:sale] = "On sale!"
+      h[:sale] = "on sale"
     else
-      h[:price] = game.search(".price").text.strip
-      h[:sale] = "Full Price"
+      h[:price] = game.search(".price").first.text.strip
+      h[:sale] = "full price"
     end
     h
   end
